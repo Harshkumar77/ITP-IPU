fa974647b16b0957866b82685cb1308e606a7e1a
mighty_volcano <kharsh110081@gmail.com>
2021-06-14 15:13:14 +0530

    g

--- 12-table/table-of-10.c
+++ 12-table/table-of-10.c
@@ -2,8 +2,10 @@
 int main()
 {
     // program to print table of 10
+    int n;
+    scanf("%d", &n);
     for (int i = 1; i <= 10; i++)
     {
-        printf("10 X %d = %d\n", i, i * 10);
+        printf("%d X %d = %d\n", n, i, i * n);
     }
 }
